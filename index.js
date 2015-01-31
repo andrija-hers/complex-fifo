7c97cda497eb1e018e1b5609a4c255da0f3c01f7
Andrija Petrovic <andrija.hers@gmail.com>
2015-01-31 13:19:34 +0100

    Init

---
+++ index.js
@@ -0,0 +1,27 @@
+var List = require('complex-list');
+
+function FIFO(){
+  List.call(this);
+  this.tail = this.head;
+}
+FIFO.prototype = Object.create(List.prototype,{constructor:{
+  value: FIFO,
+  enumerable: false,
+  configurable: false,
+  writable: false
+}});
+FIFO.prototype.destroy = function(){
+  this.tail = null;
+  List.prototype.destroy.call(this);
+};
+FIFO.prototype.push = function(obj){
+  this.add(obj);
+};
+FIFO.prototype.pop = function(){
+  var ret = this.head.content;
+  this.removeOne(this.head);
+  return ret;
+};
+
+
+module.exports = FIFO;
